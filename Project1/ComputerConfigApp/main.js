9759bdcc538c3bc31bf83f07e870fd888dd7838d
Praveer <yadav.praveer.kumar@gmail.com>
2019-04-27 11:16:18 +0800

    This is the very first application based on electron. will  be sloly improved

---
+++ Project1/ComputerConfigApp/main.js
@@ -0,0 +1,40 @@
+const {app, BrowserWindow} = require('electron')
+const path = require('path')
+const url = require('url')
+
+
+let win;
+function createWindow() {
+    // create browser window
+   
+    win = new BrowserWindow({width: 800, height: 600, icon:__dirname +'/img/sysinfo.png',
+                                webPreferences: {
+                                    nodeIntegration: true
+                                }
+                            });
+
+    // load index.html
+    win.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file',
+        slashes: true
+    }));
+
+    // open dev tool
+    win.webContents.openDevTools();
+
+    // 
+    win.on('close', () => {
+        win = null;
+    });
+    
+}
+// run create window function
+app.on('ready', createWindow);
+
+//Quit when all windows are closed
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
---
+++ Project1/ComputerConfigApp/readme.md
@@ -0,0 +1,7 @@
+
+Note : Each project needs Node_modules
+run below command 
+1. npm init
+2. npm install electron --save-dev
+To run the app run below command
+electron main.js
